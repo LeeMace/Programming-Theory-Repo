8542b2701969fbbd6da26f70782dcf1c97e6ee27
LeeMace <114292723+LeeMace@users.noreply.github.com>
2023-02-02 14:53:37 +0700

    [A] hooked up UI buttons for navigation around game

--- Principles of OOP demo/Assets/Scripts/GameManager.cs
+++ Principles of OOP demo/Assets/Scripts/GameManager.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+
+#if UNITY_EDITOR
 using UnityEngine;
+#endif
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject pauseScreen;
     private bool paused;
 
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
